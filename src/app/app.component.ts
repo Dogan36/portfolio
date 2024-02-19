39449441bdfddd7970c893bf57e2dc0db6181bd4
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-02-19 16:53:29 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ReferenceIconsComponent } from './reference-icons/reference-icons.component';
+import { SkillsComponent } from './skills/skills.component';
+import { StartingPageComponent } from './starting-page/starting-page.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutMeComponent, ContactComponent, FooterComponent, HeaderComponent, PortfolioComponent, ReferenceIconsComponent, SkillsComponent, StartingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/reference-icons/reference-icons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reference-icons',
+  standalone: true,
+  imports: [],
+  templateUrl: './reference-icons.component.html',
+  styleUrl: './reference-icons.component.scss'
+})
+export class ReferenceIconsComponent {
+
+}

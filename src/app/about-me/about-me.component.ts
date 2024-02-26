5e60615e1ad9ea897f7a8422b206a42a1548d6f6
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-02-26 18:24:43 +0100

    aboutMe finished

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { NgxTranslateModule } from '../translate/translate.module';
+import { LanguagesService } from '../languages.service';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [NgxTranslateModule],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-
+  constructor(public languagesService: LanguagesService) { }
 }

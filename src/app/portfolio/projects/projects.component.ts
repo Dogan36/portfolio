d86d67f3eed50042bb2d08bfb84fb8ec9e068a41
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-03-05 10:00:46 +0100

    portfolio finished

--- src/app/portfolio/projects/projects.component.ts
+++ src/app/portfolio/projects/projects.component.ts
@@ -1,15 +1,33 @@
 import { Component } from '@angular/core';
-import { NgFor, NgIf,} from '@angular/common';
+import { NgFor, NgIf, NgClass} from '@angular/common';
+import { NgxTranslateModule } from './../../translate/translate.module'
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgClass,  NgxTranslateModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
   projects = [
+    {
+      name: "Join",
+      link: "https://dogan-celik.developerakademie.net/join/login",
+      githublink:"https://github.com/Dogan36/ringoffire",
+      stack: ["HTML", "CSS", "JavaScript"],
+      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      imgname: "join",
+      
+    },
+    {
+      name: "El Pollo Loco",
+      link: "https://dogan-celik.developerakademie.net/el-pollo-loco/index.html",
+       githublink:"https://github.com/Dogan36/ringoffire",
+      stack: ["HTML", "CSS", "JavaScript"],
+      description : "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.",
+      imgname: "elpolloloco"
+    },
     {
       name: "Ring of Fire",
       link: "https://dogan-celik.developerakademie.net/ringoffire/",
@@ -17,33 +35,14 @@ export class ProjectsComponent {
       stack: ["HTML", "CSS", "JavaScript", "Firebase"],
       description: "testtesttest",
       imgname: "ringoffire",
-      
     },
-   /* {
-      name: "El Pollo Loco",
-      link: "https://dogan-celik.developerakademie.net/el-pollo-loco/index.html",
-      stack: ["HTML", "CSS", "JavaScript"],
-      description : "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen."
-    },
+    
     {
       name: "Pokedex",
-      link: "https://dogan-celik.developerakademie.net/pkedex/index.html",
+      link: "https://dogan-celik.developerakademie.net/pokedex/index.html",
       stack: ["HTML", "CSS", "JavaScript", "Api"],
-      description: "Based on the PokeAPI a simple library that provides and catalogues pokemon information."
+      description: "Based on the PokeAPI a simple library that provides and catalogues pokemon information.",
+      imgname: "pokedex"
     },
-    {
-      name: "Comming Soon",
-      link: "https://google.de",
-      stack:["HTMl"],
-      description: "A very Simple Customer Relationship Management system working with CRUD functionality."
-    },
-    {
-      name: "Ring of Fire",
-      link: "https://dogan-celik.developerakademie.net/ringoffire/",
-      stack: ["HTML", "CSS", "JavaScript", "Firebase"],
-      description: "testtesttest",
-      imgname: "ringoffire"
-    },*/
-
   ]
 }
